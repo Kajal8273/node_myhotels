1ebb4c63f9e75a776713ebc285c4394467edabb7
18bet <it.18bet1024@gmail.com>
2025-05-28 00:54:05 +0530

    shift mongodb to online mongodb atlas

--- db.js
+++ db.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-// Create connection
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
-mongoose.connect(mongoURL);
+
+// Create connection with local Mongodb URL-------
+  
+//const mongoURL = 'mongodb://127.0.0.1:27017/hotels'  //  const mongoURL = process.env.MONGODB_URL_LOCAL 
+ 
+// conecetd with global/hosting online db connection
+const mongoURL=process.env.MONGODB_URL;
+
+
+//setup mongodb connection
+mongoose.connect(mongoURL,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
 
 // Connection object
 const db = mongoose.connection;
